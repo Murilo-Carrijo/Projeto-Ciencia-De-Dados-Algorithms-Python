0672c07a69b21801232e552756492c0c3260514c
murilo <murilocarrijoadm@gmail.com>
2022-09-06 15:38:47 -0300

    feat: implementando a função do primeiro requisito

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,19 @@
 def study_schedule(permanence_period, target_time):
-    
-    """Faça o código aqui."""
+    counter = 0
+    if target_time is None:
+        return None
+
+    for entrice, exit in permanence_period:
+        if (
+            entrice is None
+            or exit is None
+            or not isinstance(exit, int)
+            or not isinstance(entrice, int)
+        ):
+            return None
+
+        elif entrice <= target_time <= exit:
+            counter += 1
+
+    return counter
+
