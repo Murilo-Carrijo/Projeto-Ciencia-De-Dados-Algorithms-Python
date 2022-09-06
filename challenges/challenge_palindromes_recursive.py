5a8a1dda426196ef3efb33a98c47a902114203dc
murilo <murilocarrijoadm@gmail.com>
2022-09-06 16:55:07 -0300

    feat: implementando a funcao do segundo requisito

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,11 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
+    size_word = len(word)
+    if size_word == 0:
+        return False
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+    
+    if word[low_index] == word[high_index]:
+        return True and is_palindrome_recursive(word, low_index + 1, high_index - 1)
